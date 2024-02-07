cd44c1f44547bddd70d8517d3af75464af91d617
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2024-02-07 20:19:14 +0100

    Create Commands.sh

---
+++ Commands.sh
@@ -0,0 +1,5 @@
+ls			          # list all objects in current dir
+cd <dir>		      # change directory to dir
+cd ..			        # go one level up in dir hierarchy
+touch <file.xy>		# create file.xy
+rm <file.xy>		  # remove file
