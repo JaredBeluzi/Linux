efff258a65473b01b8bb58c28bee80eaf3af0ee8
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2024-05-16 14:29:45 +0200

    Update Cronjobs.sh
    added syntax for weekdays

--- Cronjobs.sh
+++ Cronjobs.sh
@@ -7,8 +7,8 @@ su - username
 # get list of running cronjobs (each line represents a cronjob)
 crontab -l
 
-# adding a script that runs every day at 03:00 (5 Parameters are minute, hour, day of month, month, day of week)
-0 3 * * * /path/to/your/script.sh
+# adding a script that runs every weekday at 03:00 (5 Parameters are minute, hour, day of month, month, day of week)
+0 3 * * 1-5 /path/to/your/script.sh
 
 # deleting cronjob / editing cronjob file in editor
 crontab -e
